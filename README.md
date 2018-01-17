4697de73c13b61c1809cfeb69a60c8ea358b2e56
jiikko <n905i.1214@gmail.com>
2018-01-17 16:34:55 +0900

    first commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# README
+* $ bundle exec rails server -p 3000
+* $ echo "loop { system \"curl 'http://localhost:3000' 2>&1 /dev/null\" }" | irb }"
+  * wait for 1 minutes.
+* 'deadlock' is written at development.log.
---
+++ config/initializers/application_controller_renderer.rb
@@ -0,0 +1,11 @@
+# Be sure to restart your server when you modify this file.
+
+# ActiveSupport::Reloader.to_prepare do
+#   ApplicationController.renderer.defaults.merge!(
+#     http_host: 'example.org',
+#     https: false
+#   )
+# end
+#
+
+Rack::Timeout.service_timeout = 1  # seconds
